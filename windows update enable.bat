8b8e6e69468074b91e8fa23d03d68913773ce9b6
silentgameplays <fjodor.kortsagin@gmail.com>
2024-09-15 08:09:20 +0300

    Update windows update enable.bat

--- windows update enable.bat
+++ windows update enable.bat
@@ -1,8 +1,8 @@
 @echo off
 
-REM Change Windows Update Service Startup Type to Disabled (in Services)
+REM Change Windows Update Service Startup Type to Enabled (in Services)
 
-echo Disable Windows Update Startup Type
+echo Enable Windows Update Startup Type
 
 sc config "wuauserv" start= demand
 
@@ -10,14 +10,26 @@ REM can use 'auto' or 'delayed-auto' instead of 'demand makes no difference to t
 
 net start "wuauserv"
 
+echo Enable Windows Update Service
+
+REM Change Update Orchestrator Service Startup Type to Enabled (in Services)
+
+echo Enable Update Orchestrator Service 
+
 sc config "UsoSvc" start= demand
+
+REM can use 'auto' or 'delayed-auto' instead of 'demand makes no difference to this service'
 
 net start  "UsoSvc"
 
+REM Change  Windows Update Medic Service Startup Type to Enabled (in Services)
+
 sc config "WaaSMedicSvc" start= demand
+
+REM can use 'auto' or 'delayed-auto' instead of 'demand makes no difference to this service'
 
 net start "WaaSMedicSvc"
 
-echo Enable Windows Update
+echo Enable Windows Update Medic Service
 
 @echo off
